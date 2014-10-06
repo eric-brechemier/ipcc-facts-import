8a54c4d2ac8eaf3e5259316ff8b3f527c5f91028
Eric Bréchemier <medea@eric.brechemier.name>
2014-10-06 13:51:59 +0200

    Create facts table in generated script

--- import.sh
+++ import.sh
@@ -28,10 +28,33 @@ echo "Read short hash of last commit: $shortHash"
 cd ..
 
 echo "Create SQL script import.sql"
-cat << EOF > ../import.sql
+cat << EOF > import.sql
 -- create database ipcc_facts
 CREATE DATABASE IF NOT EXISTS ipcc_facts
 DEFAULT CHARACTER SET utf8
+;
+USE ipcc_facts
+
+-- create table facts
+CREATE TABLE IF NOT EXISTS facts (
+  commit CHAR(7) NOT NULL
+  COMMENT 'hash of latest commit in ipcc-fact-checking',
+  source VARCHAR(30) NOT NULL
+  COMMENT 'first-level folder name in ipcc-fact-checking',
+  document VARCHAR(40) NOT NULL
+  COMMENT 'second-level folder name in ipcc-fact-checking',
+  dataset VARCHAR(30) NOT NULL
+  COMMENT 'third-level folder name in ipcc-fact-checking',
+  line SMALLINT UNSIGNED NOT NULL
+  COMMENT 'line number in data.csv, or 0 for meta.txt',
+  name VARCHAR(30) NOT NULL
+  COMMENT 'column header in data.csv, property name in meta.txt',
+  value VARCHAR(1000) NOT NULL
+  COMMENT 'field value in data.csv, property value in meta.txt',
+  PRIMARY KEY USING HASH (
+    commit, source, document, dataset, line, name
+  )
+)
 ;
 EOF
 
